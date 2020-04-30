5c4f6d315d6c1152461235232eba750c63264942
Isaac A. Gonzalez <isaaca.gonzalez228@gmail.com>
2020-04-30 15:01:34 -0700

    initial algo commit

---
+++ algos.js
@@ -0,0 +1,21 @@
+// credit Scotch.io https://scotch.io/courses/the-ultimate-guide-to-javascript-algorithms/a-gentle-introduction-to-algorithms-and-data-structures
+
+// Array Implementation
+let user = [
+  "Obosi",
+  "Philip",
+  200,
+  "Nigeria",
+  "Frontend",
+  ["English", "Igbo", "French"],
+];
+
+// Object Implementation
+let user = {
+  firstName: "Philip",
+  lastName: "Obosi",
+  contributions: 200,
+  country: "Nigeria",
+  role: "Frontend",
+  languagesSpoken: ["English", "Igbo", "French"],
+};
